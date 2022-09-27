eb04f8d96e6ce8e4757a4897a81fed842c68952b
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-27 15:05:24 +0700

    Update injector.cpp

--- injector.cpp
+++ injector.cpp
@@ -94,6 +94,35 @@ int main() {
 	}	
 }
 
+bool SetPrivilege(LPCSTR lpszPrivilege, BOOL bEnablePrivilege = TRUE) {
+	TOKEN_PRIVILEGES priv = { 0,0,0,0 };
+	HANDLE hToken = NULL;
+	LUID luid = { 0,0x14 };
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	if (!LookupPrivilegeValue(0, lpszPrivilege, &luid)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	priv.PrivilegeCount = 1;
+	priv.Privileges[0].Luid = luid;
+	priv.Privileges[0].Attributes = bEnablePrivilege ? SE_PRIVILEGE_ENABLED : SE_PRIVILEGE_REMOVED;
+	if (!AdjustTokenPrivileges(hToken, false, &priv, 0, 0, 0)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	if (hToken)
+		CloseHandle(hToken);
+	return true;
+}
+
+
+
 DWORD Injector::GetProcessIdByWindow(const std::tstring& Name)
 {
 	// Find window
