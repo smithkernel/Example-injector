4fc24c3174e9c83eff722b745f43ec29f9739823
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-27 15:05:57 +0700

    Update Loader.hpp

--- Injector/Loader.hpp
+++ Injector/Loader.hpp
@@ -1,72 +1,75 @@
+#include "Loader.hpp"
 
-public:
-	Get_SSDT()
-	{
-		GetKeServiceDescriptorTableAddrX64();
-	}
-	~Get_SSDT() = default;
-public:
-	ULONG64 GetSSDTFuncCurAddrByIndex(ULONG index)
-	{
-		LONG dwtmp = 0;
-		ULONGLONG addr = 0;
-		PULONG ServiceTableBase = NULL;
-		ServiceTableBase = (PULONG)KeServiceDescriptorTable->ServiceTableBase;
-		dwtmp = ServiceTableBase[index];
-		dwtmp = dwtmp >> 4;
-		addr = ((LONGLONG)dwtmp + (ULONGLONG)ServiceTableBase);//&0xFFFFFFF0;
-		return addr;
-	}
 
-	KIRQL WPOFFx64()
-	{
-		KIRQL irql = KeRaiseIrqlToDpcLevel();
-		UINT64 cr0 = __readcr0();
-		cr0 &= 0xfffffffffffeffff;
-		__writecr0(cr0);
-		_disable();
-		return irql;
+Logger::Logger(wstring filename)
+	:logText(NULL), DoLog(false), filename(filename)
+{
+}
+
+
+Logger::~Logger()
+{
+	if (logText)
+		CloseHandle(logText);
+}
+
+void Logger::startLog()
+{
+	if (!logText) {
+		wstring filepathBase = L"C:\\Users\\Hunter\\Desktop\\Logfiles\\" + filename + L".txt";
+		logText = CreateFileW(filepathBase.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	}
 
-	void WPONx64(KIRQL irql)
-	{
-		UINT64 cr0 = __readcr0();
-		cr0 |= 0x10000;
-		_enable();
-		__writecr0(cr0);
-		KeLowerIrql(irql);
-	}
-public:
-	PSYSTEM_SERVICE_TABLE KeServiceDescriptorTable = nullptr;
-private:
-	void GetKeServiceDescriptorTableAddrX64()
-	{
-		PUCHAR StartSearchAddress = (PUCHAR)__readmsr(0xC0000082);
-		PUCHAR EndSearchAddress = StartSearchAddress + 0x500;
-		PUCHAR i = NULL;
-		UCHAR b1 = 0, b2 = 0, b3 = 0;
-		ULONGLONG templong = 0;
-		ULONGLONG addr = 0;
-		for (i = StartSearchAddress; i < EndSearchAddress; i++)
-		{
-			if (MmIsAddressValid(i) && MmIsAddressValid(i + 1) && MmIsAddressValid(i + 2))
-			{
-				b1 = *(i);
-				b2 = *(i + 1);
-				b3 = *(i + 2);
-				if (b1 == 0x4c && b2 == 0x8d && b3 == 0x15)
-				{
-					memcpy(&templong, i + 3, 4);
-					//���Ĳ���  
-					//kd> db fffff800`03e8b772  
-					//fffff800`03e8b772  4c 8d 15 c7 20 23 00 4c-8d 1d 00 21 23 00 f7 83  L... #.L...!#...  
-					//templong = 002320c7 ,i = 03e8b772, 7Ϊָ���  
-					addr = (ULONGLONG)templong + (ULONGLONG)i + 7;
-					break;
-				}
-			}
-		}
-		KeServiceDescriptorTable = (PSYSTEM_SERVICE_TABLE)addr;
-	}
-};
+	DoLog = true;
+}
 
+void Logger::stopLog()
+{
+	DoLog = false;
+}
+
+string Logger::LogString(string explaination, const string str)
+{
+	if (DoLog && logText) {
+		string logme = "Log_string : " + explaination + " ! " + str + "\r\n" ;
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return str;
+}
+
+float Logger::LogFloat(string explaination, const float value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_Float : " + explaination + " ! " + tostr<float>(value) + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+
+int Logger::LogInt(string explaination, const int value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_Int : " + explaination + " ! " + tostr<int>(value) + "\r\n" ;
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+BYTE Logger::LogBYTE(string explaination, const BYTE value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_BYTE : " + explaination + " ! " + tostr<BYTE>(value) + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+
+DWORD64 Logger::LogAddress(string explaination, const DWORD64 value)
+{
+	if (DoLog && logText) {
+		ostringstream os;
+		os << hex << value;
+		string logme = "Log_Address : " + explaination + " ! "  + "0x" + os.str() + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
