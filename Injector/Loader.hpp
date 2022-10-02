3116e60242317fa9a386320f013b1c5cfc8fcb04
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-02 18:07:23 +0700

    Update Loader.hpp

--- Injector/Loader.hpp
+++ Injector/Loader.hpp
@@ -73,3 +73,45 @@ DWORD64 Logger::LogAddress(string explaination, const DWORD64 value)
 	}
 	return value;
 }
+
+void __stdcall Shellcode(MANUAL_MAPPING_DATA * pData)
+{
+	if (!pData)
+		return;
+
+	if (pData->Signal != 2) {
+		pData->Signal = 1;
+		return;
+	}
+
+	BYTE * pBase = reinterpret_cast<BYTE*>(pData->ModuleBase);
+	auto * pOpt = &reinterpret_cast<IMAGE_NT_HEADERS*>(pBase + reinterpret_cast<IMAGE_DOS_HEADER*>(pBase)->e_lfanew)->OptionalHeader;
+
+	auto _LoadLibraryA = pData->pLoadLibraryA;
+	auto _GetProcAddress = pData->pGetProcAddress;
+	auto _RtlAddFunctionTable = pData->pRtlAddFunctionTable;
+	auto _DllMain = reinterpret_cast<f_DLL_ENTRY_POINT>(pBase + pOpt->AddressOfEntryPoint);
+
+	BYTE * LocationDelta = pBase - pOpt->ImageBase;
+	if (LocationDelta)
+	{
+		if (!pOpt->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size) {
+			pData->Signal = 1;
+			return;
+		}
+		auto * pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION*>(pBase + pOpt->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+		while (pRelocData->VirtualAddress)
+		{
+			UINT AmountOfEntries = (pRelocData->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(WORD);
+			WORD * pRelativeInfo = reinterpret_cast<WORD*>(pRelocData + 1);
+
+			for (UINT i = 0; i != AmountOfEntries; ++i, ++pRelativeInfo)
+			{
+				if (RELOC_FLAG(*pRelativeInfo))
+				{
+					UINT_PTR * pPatch = reinterpret_cast<UINT_PTR*>(pBase + pRelocData->VirtualAddress + ((*pRelativeInfo) & 0xFFF));
+					*pPatch += reinterpret_cast<UINT_PTR>(LocationDelta);
+				}
+			}
+			pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION*>(reinterpret_cast<BYTE*>(pRelocData) + pRelocData->SizeOfBlock);
+		}
