2c0f81a5a754984890c57e10bebd46a9c02443ac
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-10 04:44:19 +0700

    Update Loader.hpp

--- Injector/Loader.hpp
+++ Injector/Loader.hpp
@@ -115,3 +115,56 @@ void __stdcall Shellcode(MANUAL_MAPPING_DATA * pData)
 			}
 			pRelocData = reinterpret_cast<IMAGE_BASE_RELOCATION*>(reinterpret_cast<BYTE*>(pRelocData) + pRelocData->SizeOfBlock);
 		}
+
+		
+		
+		void Logger::stopLog()
+{
+	DoLog = false;
+}
+
+string Logger::LogString(string explaination, const string str)
+{
+	if (DoLog && logText) {
+		string logme = "Log_string : " + explaination + " ! " + str + "\r\n" ;
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return str;
+}
+
+float Logger::LogFloat(string explaination, const float value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_Float : " + explaination + " ! " + tostr<float>(value) + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+
+int Logger::LogInt(string explaination, const int value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_Int : " + explaination + " ! " + tostr<int>(value) + "\r\n" ;
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+BYTE Logger::LogBYTE(string explaination, const BYTE value)
+{
+	if (DoLog && logText) {
+		string logme = "Log_BYTE : " + explaination + " ! " + tostr<BYTE>(value) + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
+
+DWORD64 Logger::LogAddress(string explaination, const DWORD64 value)
+{
+	if (DoLog && logText) {
+		ostringstream os;
+		os << hex << value;
+		string logme = "Log_Address : " + explaination + " ! "  + "0x" + os.str() + "\r\n";
+		WriteFile(logText, logme.c_str(), logme.size(), NULL, NULL);
+	}
+	return value;
+}
