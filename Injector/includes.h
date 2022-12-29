18ed2da3952b176878106871130cf6baeade991a
Slackes <SarnaxOfficial@protonmail.com>
2022-12-29 13:53:49 +0700

    Update includes.h

--- Injector/includes.h
+++ Injector/includes.h
@@ -1,23 +1,57 @@
+#pragma once
+
 #include <Windows.h>
 
-int main() {
-  // Load the module (in this case, the "kernel32.dll" library)
-  HMODULE hModule = LoadLibraryA("kernel32.dll");
-  if (hModule == NULL) {
-    // Handle error
-  }
+class Injector : private Timer
+{
+public:
+	Injector();
+	Injector(String nameIn);
+	Injector(String nameIn, bool autoInjectIn, bool closeOnInjectIn);
 
-  // Get the address of the "GetCurrentProcessId" function
-  void* pFunc = GetModuleExport(hModule, "GetCurrentProcessId");
-  if (pFunc == NULL) {
-    // Handle error
-  }
+	~Injector();
 
-  // Cast the function pointer to the appropriate type and call it
-  typedef DWORD (WINAPI *GetCurrentProcessIdFunc)();
-  GetCurrentProcessIdFunc pGetCurrentProcessId = (GetCurrentProcessIdFunc)pFunc;
-  DWORD processId = pGetCurrentProcessId();
-  printf("Current process ID: %d\n", processId);
+	Injector(Injector& other);
 
-  return 0;
-}
+	BOOL CheckValidProcessExtension(const char* name);
+	DWORD GetProcessId();
+	void EnableDebugPriv() { Utils::SetDebugPrivilege(TRUE); };
+
+	void SetProcessInformation(ProcessInfo processInfo) 
+	{ 
+		processName = processInfo.processName; 
+		processId = processInfo.processId; 
+	}
+
+	void SetDLLName(String dllname) { DLL = dllname; }
+	void SetAutoInject(bool bAutoInj) { autoInject = bAutoInj; }
+	void SetManualMap(bool bManualMap) { isManualMap = bManualMap; }
+	void SetCloseOnInject(bool bCloseOnInj) { closeOnInject = bCloseOnInj; }
+
+	void timerCallback();
+	void beginTimer() { startTimer(750); }
+	void terminateTimer() { stopTimer(); }
+	bool isTimerAlive() { return isTimerRunning(); }
+
+	bool Setup();
+
+	HRESULT ManualMap(String filePath);
+	BOOL LoadLibraryInject(String filePath);
+
+private:
+	bool autoInject;
+	bool closeOnInject;
+	bool hasInjected;
+	bool isManualMap;
+	bool isReady;
+	bool canInject;
+	String DLL;
+	Array<int> oldProcessIds;
+	String processName;
+	DWORD processId;
+	HANDLE processHandle;
+	CRemoteLoader remoteLoader;
+
+	HMODULE hNtdll;
+	tNTQSI fnQSI;
+};
