fa4051673cecb512789f078359b9e247413194a1
Slackes <SarnaxOfficial@protonmail.com>
2022-12-31 04:07:23 +0700

    Update functions.h

--- Injector/functions.h
+++ Injector/functions.h
@@ -118,3 +118,32 @@ namespace processUtils
         return modules;
     }
 }
+
+class ProcessesComponent : public Component, public ButtonListener
+{
+public:
+	ProcessesComponent();
+	~ProcessesComponent();
+
+    void paint(Graphics& g);
+    void resized();
+    void buttonClicked(Button* buttonThatWasClicked);
+	bool FetchProcessList();
+	ProcessInfo getCurrentProcess();
+	TableComponent* getProcessList();
+
+private:
+    TableComponent processList;
+
+    ScopedPointer<TextButton> okButton;
+
+	LookAndFeelCustom lookAndFeelCustom;
+	
+	Array<ProcessInfo> processes;
+
+	HMODULE hNtdll;
+	tNTQSI fnQSI;
+
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ProcessesComponent)
+};
