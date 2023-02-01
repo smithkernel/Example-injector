2ce86c2089e98bfa6f8490f81aa7a6992594f8c0
Slackes <SarnaxOfficial@protonmail.com>
2023-02-02 01:14:30 +0700

    C++ DLL Injection Code
    This version of the code includes error checking and proper error handling, which will allow you to identify any issues that may occur during the injection process. Additionally, it uses std::cerr to output error messages instead of a message box.

--- Injector/Loader.hpp
+++ Injector/Loader.hpp
@@ -1,33 +1,56 @@
 #include "Loader.hpp"
-#include <mutex>
+#include <iostream>
+#include <string>
+#include <Windows.h>
 
-int main()
-
+int main() {
     DWORD procc_id = NULL;
     char dll_path[MAX_PATH];
-    const char* dll_Name;
-    const char* win_Name;
     std::string auxDll;
     std::string auxWin;
 
-    std::cout << "Ingresa el nombre del archivo DLL" << std::endl;
+    std::cout << "Enter the name of the DLL file: " << std::endl;
     std::cin >> auxDll;
-    std::cout << "Ingresa el nombre de la ventana a inyectar" << std::endl;
+    std::cout << "Enter the name of the window to inject: " << std::endl;
     std::cin >> auxWin;
-    dll_Name = auxDll.c_str(); //Convert string to const char*
-    win_Name = auxWin.c_str();
+    const char* dll_Name = auxDll.c_str(); // Convert string to const char*
+    const char* win_Name = auxWin.c_str();
 
-    GetFullPathName((LPCWSTR)dll_Name, MAX_PATH, (LPWSTR)dll_path, nullptr);
+    GetFullPathName(auxDll.c_str(), MAX_PATH, dll_path, NULL);
     injectorUtils::get_process_id(win_Name, procc_id);
-    HANDLE h_process = OpenProcess(PROCESS_ALL_ACCESS, NULL, procc_id);
-    void* allocate_memory = VirtualAllocEx(h_process, nullptr, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    WriteProcessMemory(h_process, allocate_memory, dll_path, MAX_PATH, nullptr);
-    HANDLE h_thread = CreateRemoteThread(h_process, nullptr, NULL, LPTHREAD_START_ROUTINE(LoadLibraryA), allocate_memory, NULL, nullptr);
+    HANDLE h_process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procc_id);
+    if (h_process == NULL) {
+        std::cerr << "Failed to open process." << std::endl;
+        return 1;
+    }
+    void* allocate_memory = VirtualAllocEx(h_process, NULL, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (allocate_memory == NULL) {
+        std::cerr << "Failed to allocate memory in the target process." << std::endl;
+        CloseHandle(h_process);
+        return 1;
+    }
+    if (WriteProcessMemory(h_process, allocate_memory, dll_path, MAX_PATH, NULL) == FALSE) {
+        std::cerr << "Failed to write to process memory." << std::endl;
+        VirtualFreeEx(h_process, allocate_memory, 0, MEM_RELEASE);
+        CloseHandle(h_process);
+        return 1;
+    }
+    HANDLE h_thread = CreateRemoteThread(h_process, NULL, 0, (LPTHREAD_START_ROUTINE) LoadLibraryA, allocate_memory, 0, NULL);
+    if (h_thread == NULL) {
+        std::cerr << "Failed to create remote thread." << std::endl;
+        VirtualFreeEx(h_process, allocate_memory, 0, MEM_RELEASE);
+        CloseHandle(h_process);
+        return 1;
+    }
+    WaitForSingleObject(h_thread, INFINITE);
+    VirtualFreeEx(h_process, allocate_memory, 0, MEM_RELEASE);
+    CloseHandle(h_thread);
     CloseHandle(h_process);
-    VirtualFreeEx(h_process, allocate_memory, NULL, MEM_RESERVE);
-    MessageBox(NULL, (LPCWSTR)"Exito", (LPCWSTR)"Inyeccion exitosa",NULL);
+
+    std::cout << "Injection successful." << std::endl;
     return 0;
 }
+
 
 #include <fstream>
 #include <mutex>
