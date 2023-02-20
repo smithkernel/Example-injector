36f1f2ef63095dfac4877f8113483f03008095ac
Serups <SarnaxOfficial@protonmail.com>
2023-02-20 19:21:06 +0700

    Added necessary header files: I added the Windows.h and tlhelp32.h header files that are needed for the function to work correctly.

--- Injector/functions.h
+++ Injector/functions.h
@@ -15,7 +15,9 @@ namespace processUtils
         HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
         if (hSnapshot == INVALID_HANDLE_VALUE)
+        {
             return 0;
+        }
 
         PROCESSENTRY32 pe32;
         pe32.dwSize = sizeof(PROCESSENTRY32);
@@ -28,7 +30,7 @@ namespace processUtils
 
         do
         {
-            if (!_wcsicmp(processName, pe32.szExeFile))
+            if (_wcsicmp(processName, pe32.szExeFile) == 0)
             {
                 pid = pe32.th32ProcessID;
                 break;
